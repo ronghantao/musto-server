3105b853317560e19a4c37a4ce5cc1406d9da8ad
ronghantao <ronghantao@yeah.net>
2014-08-27 21:06:56 +0800

    update

--- app/controllers/Verify.java
+++ app/controllers/Verify.java
@@ -23,6 +23,6 @@ public class Verify extends BaseController {
 			@Required @Min(0) @Max(3) Integer loginType) {
 		checkParams();
 		Person p = LoginService.newInstance(loginId, pwdMd5, loginType).checkUserGet();
-        renderJSON(onSuccess(p.getJson()));
+        renderJSON(onSuccess(p.getObj()));
     }
 }
--- app/model/Teacher.java
+++ app/model/Teacher.java
@@ -3,8 +3,4 @@ package model;
 import com.google.gson.Gson;
 
 public class Teacher extends Person{
-	public String getJson(){
-		String p = super.getJson();
-		return new Gson().toJson(this);
-	}
 }
